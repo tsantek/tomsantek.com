c36927f494bb9b4c00865a3b4e22678b06728f96
tsantek <santek.vg@gmail.com>
2019-05-12 19:58:42 -0700

    update blog

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 // how many days from lest blog...
-function days_passed(dt) {
+function daysPassedOne(dt) {
     var current = new Date(dt.getTime());
     var previous = new Date(dt.getFullYear(), 4, 10);
     return Math.ceil((current - previous + 1) / 86400000);
 }
 
-document.querySelector('#time-of-post').innerHTML = "Posted " + days_passed(new Date()) + " days ago";
+document.querySelector('#time-of-post').innerHTML = "Posted " + daysPassedOne(new Date()) + " days ago";
+
+function daysPassedTwo(dt) {
+    var current = new Date(dt.getTime());
+    var previous = new Date(dt.getFullYear(), 4, 12);
+    return Math.ceil((current - previous + 1) / 86400000);
+}
+
+document.querySelector('#time-of-post-page-two').innerHTML = "Posted " + daysPassedTwo(new Date()) + " days ago";
